f9fb9579be31a9fbfdb1b2a0239cbfd2af186846
Bartłomiej Słota <bartlomiej.slota@gmail.com>
2024-03-19 21:18:40 +0100

    VOs replaced with records

--- simple-availability/src/main/java/com/bartslota/availability/domain/OwnerId.java
+++ simple-availability/src/main/java/com/bartslota/availability/domain/OwnerId.java
@@ -1,34 +1,9 @@
 package com.bartslota.availability.domain;
 
-import java.util.Objects;
-
-public class OwnerId {
-
-    private final String value;
-
-    private OwnerId(String value) {
-        this.value = value;
-    }
+public record OwnerId(String value) {
 
     public static OwnerId of(String value) {
         return new OwnerId(value);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof OwnerId)) {
-            return false;
-        }
-        OwnerId ownerId = (OwnerId) o;
-        return Objects.equals(value, ownerId.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(value);
     }
 
     public String asString() {
