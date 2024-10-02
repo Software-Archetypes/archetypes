8e51ad608d14622fb58cdaee5d0bc26b6a7eab9d
Ignacy Szreter <ignacyszreter@gmail.com>
2024-10-02 15:05:57 +0200

    fix tests

--- configurator/src/main/java/softwarearchetypes/sat/DPLLSolver.java
+++ configurator/src/main/java/softwarearchetypes/sat/DPLLSolver.java
@@ -1,9 +1,6 @@
 package softwarearchetypes.sat;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 public class DPLLSolver {
 
@@ -47,5 +44,15 @@ public class DPLLSolver {
         assignmentWithFalse.put(variable, false);
 
         return solve(clauses, assignmentWithFalse);
+    }
+
+    public boolean isClauseSatisfied(List<Clause> clauses, Set<Integer> trueLiterals) {
+        return clauses.stream().allMatch(clause ->
+                clause.representation().stream().anyMatch(literal ->
+                        (trueLiterals.contains(Math.abs(literal)) && literal > 0)
+                                ||
+                                (literal < 0 && !trueLiterals.contains(Math.abs(literal)))
+                )
+        );
     }
 }
